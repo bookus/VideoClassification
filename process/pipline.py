df1425db20b633af4cb3b2d396c3e2afab5accb6
liushuchun <liuscgood@gmail.com>
2017-08-08 18:28:18 +0800

    good

--- process/pipline.py
+++ process/pipline.py
@@ -18,13 +18,10 @@ train_val_path = "/workspace/data/video/videos/trainval"
 
 test_path = "/workspace/data/video/videos/test"
 
-
 trainval_map = {}
 
 
 def init():
-
-
 
 
 def split(line):
@@ -32,20 +29,21 @@ def split(line):
 
 
 class Consumer(multiprocessing.Process):
-    def __init__(self, task_queue, result_queue,ID):
+    def __init__(self, task_queue, result_queue, ID):
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
         self.result_queue = result_queue
-        self.ID=ID
-        self.temp_path="./temp"+str(ID)
+        self.ID = ID
+        self.temp_path = "./temp" + str(ID)
         os.mkdir(self.temp_path)
 
-
-    def upload(self,file):
-        file_path=self.temp_path+"/"+file
-
+    def upload(self, file, key):
+        file_path = self.temp_path + "/" + file
+        cmd = './qrsctl put -c {}  '
         os.system()
 
+    def clean(self):
+        pass
 
     def run(self):
         proc_name = self.name
@@ -57,21 +55,21 @@ class Consumer(multiprocessing.Process):
                 self.task_queue.task_done()
                 break
 
-            file_path=os.path.join(train_val_path,file)
+            file_path = os.path.join(train_val_path, file)
 
-            cmd='./export_frames -i {} -interval 4 -c 21 -o {} -s 256x256 -postfix jpg'.format(file_path,self.temp_path)
+            cmd = './export_frames -i {} -interval 4 -c 21 -o {} -s 256x256 -postfix jpg'.format(file_path,
+                                                                                                 self.temp_path)
             print(cmd)
-            #执行算光流
+            # 执行算光流
             os.system(cmd)
 
-            up_files=os.listdir(self.temp_path)
+            up_files = os.listdir(self.temp_path)
 
             for file in up_files:
-                #/[test/train/val]/[label]/[filename][frame/flow][序列].jpg
+            # /[test/train/val]/[label]/[filename][frame/flow][序列].jpg
 
 
 
-                os.system()
 
             self.task_queue.task_done()
             self.result_queue.put(answer)
@@ -83,7 +81,6 @@ def producer():
 
     for file in trainval_files:
         yield file
-
 
 
 def main():
@@ -107,7 +104,7 @@ def main():
     # 开始 消费
     num_consumers = multiprocessing.cpu_count() * 2
 
-    consumers = [Consumer(tasks, results,i)
+    consumers = [Consumer(tasks, results, i)
                  for i in range(num_consumers)]
     for c in consumers:
         c.start()
@@ -119,7 +116,6 @@ def main():
 
     # Wait for all of the tasks to finish
     tasks.join()
-
 
 
 if __name__ == "__main__":
