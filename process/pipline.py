855fc04084feaf4334934cfc7d60c068622401d3
liushuchun <liuscgood@gmail.com>
2017-08-08 17:03:08 +0800

    fix

--- process/pipline.py
+++ process/pipline.py
@@ -11,34 +11,64 @@ ak = "8MbTywnGQZ75BnWL9S1P8PZn-9wCqy6fIs4MyllI"
 sc = "XXXXXXX"
 
 train_path = "lsvc2017/lsvc_train.txt"
-val_path = "lsvc2017/lsvc_val.txt"
 
+val_path = "lsvc2017/lsvc_val.txt"
 
 train_val_path = "/workspace/data/video/videos/trainval"
 
+test_path = "/workspace/data/video/videos/test"
+
+
 trainval_map = {}
+
+
+def init():
+
+
+
 
 def split(line):
     return line.split(',', 1)
 
 
 class Consumer(multiprocessing.Process):
-    def __init__(self, task_queue, result_queue):
+    def __init__(self, task_queue, result_queue,ID):
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
         self.result_queue = result_queue
+        self.ID=ID
+        self.temp_path="./temp"+str(ID)
+        os.mkdir(self.temp_path)
+
+
+    def upload(self,file):
+        file_path=self.temp_path+"/"+file
+
+        os.system()
+
 
     def run(self):
         proc_name = self.name
         while True:
-            next_task = self.task_queue.get()
-            if next_task is None:
+            file = self.task_queue.get()
+            if file is None:
                 # 可以退出
 
                 self.task_queue.task_done()
                 break
 
-            answer = next_task()
+            file_path=os.path.join(train_val_path,file)
+
+            cmd='./export_frames -i {} -interval 4 -c 21 -o {} -s 256x256 -postfix jpg'.format(file_path,self.temp_path)
+            print(cmd)
+
+            os.system(cmd)
+
+            up_files=os.listdir(self.temp_path)
+
+
+            os.system()
+
             self.task_queue.task_done()
             self.result_queue.put(answer)
         return
@@ -73,7 +103,7 @@ def main():
     # 开始 消费
     num_consumers = multiprocessing.cpu_count() * 2
 
-    consumers = [Consumer(tasks, results)
+    consumers = [Consumer(tasks, results,i)
                  for i in range(num_consumers)]
     for c in consumers:
         c.start()
@@ -86,7 +116,6 @@ def main():
     # Wait for all of the tasks to finish
     tasks.join()
 
-    os.system('./export_frames -i lsvc058603.avi -interval 4 -c 21 -o ./pics -s 256x256 -postfix jpg')
 
 
 if __name__ == "__main__":
